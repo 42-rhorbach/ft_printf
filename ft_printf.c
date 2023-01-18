29799f1681c7c1c508cb7ad28ea92d1dac97782f
QuiteHollow <robert123horbach@gmail.com>
2023-01-18 16:48:35 +0100

    3 convs work, chars between flags are written

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rhorbach <rhorbach@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/09 14:14:42 by rhorbach      #+#    #+#                 */
-/*   Updated: 2023/01/17 16:41:31 by rhorbach      ########   odam.nl         */
+/*   Updated: 2023/01/18 16:43:05 by rhorbach      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,70 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int	conv_c(va_list ap)
+int	conv_c(va_list ap) //write can fail
 {
 	char	c;
 
 	c = va_arg(ap, char);
-	write(1, &c, 1);
+	ft_putchar_fd(c, STDOUT_FILENO);
 	return (0);
 }
 
 int	conv_s(va_list ap)
 {
-	char *s = va_arg(ap, char*);
+	char	*s;
+
+	s = va_arg(ap, char *);
+	ft_putstr_fd(s, STDOUT_FILENO);
 	return (0);
 }
 
-int	conv_p(va_list ap)
+int	conv_p(va_list ap) //WIP
 {
-	void *p = va_arg(ap, void *);
+	void	*p;
+
+	p = va_arg(ap, void *);
 	return (0);
 }
 
 int	conv_i(va_list ap)
 {
-	int i = va_arg(ap, int);
-	printf("%i\n", i);
+	int		i;
+
+	i = va_arg(ap, int);
+	ft_putstr_fd(ft_itoa(i), STDOUT_FILENO);
 	return (0);
 }
 
-int	conv_u(va_list ap)
+int	conv_u(va_list ap) //WIP
 {
-	unsigned int u = va_arg(ap, unsigned int);
+	unsigned int	u;
+
+	u = va_arg(ap, unsigned int);
+	ft_putstr_fd(ft_itoa(u), STDOUT_FILENO);
 	return (0);
 }
 
-int	conv_lowerx(va_list ap)
+int	conv_lowerx(va_list ap) //WIP
 {
-	unsigned int x = va_arg(ap, unsigned int);
+	unsigned int	x;
+
+	x = va_arg(ap, unsigned int);
 	return (0);
 }
 
-int	conv_upperx(va_list ap)
+int	conv_upperx(va_list ap) //WIP
 {
-	unsigned int x = va_arg(ap, unsigned int);
+	unsigned int	x;
+
+	x = va_arg(ap, unsigned int);
 	return (0);
 }
 
-int	conv_percent(va_list ap)
+int	conv_percent(va_list ap) //WIP
 {
 	// (void)ap;
-	//
+	//ft_putchar_fd('%');
 	return (0);
 }
 
@@ -110,10 +124,7 @@ int	ft_printf(const char *fmt, ...)
 			// if return value != 0 error
 		}
 		else
-		{
-
-		}
-
+			ft_putchar_fd(fmt[i], STDOUT_FILENO);
 		i++;
 	}
 	va_end(ap);
@@ -121,6 +132,6 @@ int	ft_printf(const char *fmt, ...)
 
 int main (void)
 {
-	ft_printf("%c hello %i", 'a', 457, "jahsd");
-
+	ft_printf("%c hello %s %i %u %x \n", 'a', "bye", 346, -200, 0xaf54a);
+	printf("%c hello %s %i %u %x \n", 'a', "bye", 346, -200, 0xaf54a);
 }
